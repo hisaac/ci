293b8aefc8518c5affd7166f23bff94a84f23135
Isaac Halvorson <hello@hisaac.net>
2025-05-23 12:25:19 -0500

    Refactor create-vm script to only create base VM
    Signed-off-by: Isaac Halvorson <hello@hisaac.net>

--- mise/tasks/create-vm.bash
+++ mise/tasks/create-vm.bash
@@ -1,51 +1,32 @@
 #!/usr/bin/env bash
+
 #MISE description="Downloads requests Xcode versions and kicks off a Packer build for Sonoma using Tart"
 #USAGE flag "--macos-version <macos_version>" {
 #USAGE 	help    "The macOS version to use to create the VM image"
-#USAGE 	default "latest"
-#USAGE }
-#USAGE flag "--xcode-versions <xcode_versions>" {
-#USAGE 	help    "A comma separated list of the Xcode versions to install on the VM image"
-#USAGE }
-#USAGE flag "--simulator-runtimes <simulator_runtimes>" {
-#USAGE 	help    "A comma separated list of the simulator runtimes to install on the VM image <iOS|watchOS|tvOS|visionOS>"
+#USAGE 	default "15"
 #USAGE }
 
-set -euo pipefail
+set -o errexit  # Exit on error
+set -o nounset  # Exit on unset variable
+set -o pipefail # Exit on pipe failure
 IFS=$'\n\t'
 
+if [[ "${TRACE:-}" == true ]]; then
+	set -o xtrace # Trace the execution of the script (debug)
+fi
+
 function main() {
-	local -r packer_template="${MISE_PROJECT_ROOT}/templates/macos.base.pkr.hcl"
-
-	# download_xcode_versions "${usage_xcode_versions:---latest}"
-
-	usage_macos_version=">= 14"
-	usage_xcode_versions='15.1.0,15.4.0'
-	usage_simulator_runtimes='iOS'
-
+	declare -r packer_var_file="${MISE_PROJECT_ROOT}/templates/macos-${usage_macos_version}.pkrvars.hcl"
+	declare -r packer_template="${MISE_PROJECT_ROOT}/templates/macos.base.pkr.hcl"
 	packer init -upgrade "${packer_template}"
-	packer build \
-		-var "macos_version=\"${usage_macos_version}\"" \
-		-var "xcode_versions=\"[${usage_xcode_versions}]\"" \
-		-var "simulator_runtimes=\"[${usage_simulator_runtimes}]\"" \
-		"${packer_template}"
+	packer build -var-file="${packer_var_file}" "${packer_template}"
 }
 
-function download_xcode_versions() {
-	local -a xcode_versions
-	IFS=',' read -ra xcode_versions <<< "${1}"
-	readonly xcode_versions
-
-	local -r xcodes_download_dir="${MISE_PROJECT_ROOT}/xcodes"
-	rm -rf "${xcodes_download_dir}"
-	mkdir -p "${xcodes_download_dir}"
-
-	xcodes update >/dev/null
-
-	for version in "${xcode_versions[@]}"; do
-		echo "Downloading Xcode ${version}"
-		xcodes download "$version" --directory "${xcodes_download_dir}"
-	done
+trap exit_handler EXIT
+function exit_handler() {
+	declare -ri exit_code="$?"
+	declare -r script_name="${0##*/}"
+	echo -e "\n==> ${script_name} exited with code ${exit_code}"
 }
 
 main "$@"
