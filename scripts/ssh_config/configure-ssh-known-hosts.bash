fdd6a5846d59d8db2d829191de64fccc27520c87
Isaac Halvorson <hello@hisaac.net>
2026-05-11 19:29:45 -0500

    migrate github ssh known hosts setup

--- scripts/ssh_config/configure-ssh-known-hosts.bash
+++ scripts/ssh_config/configure-ssh-known-hosts.bash
@@ -3,7 +3,7 @@
 function main() {
 	echo "Adding GitHub to known hosts..."
 	mkdir -p "$HOME/.ssh"
-	chmod 777 "$HOME/.ssh"
+	chmod 0700 "$HOME/.ssh"
 	cat >>"$HOME/.ssh/known_hosts" <<-EOF
 		# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
 		github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
