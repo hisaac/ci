0feed9c2717e71c170c8fb032e81b27f0bc63a32
Isaac Halvorson <hello@hisaac.net>
2026-05-13 13:31:43 -0500

    Check for updates to xcode clts
    Signed-off-by: Isaac Halvorson <hello@hisaac.net>

--- roles/xcode_clt/files/install-clt.bash
+++ roles/xcode_clt/files/install-clt.bash
@@ -2,23 +2,25 @@
 
 declare placeholder_file="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
 touch "${placeholder_file}"
+trap 'rm -f "${placeholder_file}"' EXIT
 
-echo "🔎 Finding latest Command Line Tools..."
+echo "🔎 Checking for Command Line Tools updates..."
 declare clt_label=""
-while [[ -z "${clt_label}" ]]; do
-	clt_label="$(
-		softwareupdate --list 2>/dev/null |
-			grep --after-context=1 '\* Label: Command Line Tools' |
-			paste - - |
-			sed 's/.*\* Label: //; s/\tTitle:.*Version: /\t/' |
-			sort --field-separator=$'\t' --key=2 --version-sort |
-			tail -n1 |
-			cut -f1
-	)"
-done
+clt_label="$(
+	softwareupdate --list 2>/dev/null |
+		grep --after-context=1 '\* Label: Command Line Tools' |
+		paste - - |
+		sed 's/.*\* Label: //; s/\tTitle:.*Version: /\t/' |
+		sort --field-separator=$'\t' --key=2 --version-sort |
+		tail -n1 |
+		cut -f1
+)"
+
+if [[ -z "${clt_label}" ]]; then
+	echo "✅ Command Line Tools are already up to date."
+	exit 0
+fi
 
 echo "📦 Installing ${clt_label}..."
 softwareupdate --install --no-scan --verbose "${clt_label}"
 xcode-select --switch "/Library/Developer/CommandLineTools"
-
-rm -f "${placeholder_file}"
