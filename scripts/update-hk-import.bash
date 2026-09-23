a7440bb295006664b723bc7aba8325bed6ba2428
Isaac Halvorson <hello@hisaac.net>
2026-09-23 08:32:42 -0500

    fix hk update script

--- scripts/update-hk-import.bash
+++ scripts/update-hk-import.bash
@@ -3,8 +3,8 @@
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/lib/common.bash" || exit 1
 
 # Example import headers to be updated:
-# amends "package://github.com/jdx/hk/releases/download/v1.34.0/hk@1.34.0#/Config.pkl"
-# import "package://github.com/jdx/hk/releases/download/v1.34.0/hk@1.34.0#/Builtins.pkl"
+# amends "package://github.com/jdx/hk/releases/download/v2.0.1/hk@2.0.1#/Config.pkl"
+# import "package://github.com/jdx/hk/releases/download/v2.0.1/hk@2.0.1#/Builtins.pkl"
 
 function main() {
 	local -r config_path="${PROJECT_ROOT}/hk.pkl"
@@ -17,14 +17,10 @@ function main() {
 
 	log_info "Updating hk imports in ${config_path} to version ${hk_version}"
 
-	local -r temp_path="$(mktemp)"
-	{
-		echo "amends \"package://github.com/jdx/hk/releases/download/v${hk_version}/hk@${hk_version}#/Config.pkl\""
-		echo "import \"package://github.com/jdx/hk/releases/download/v${hk_version}/hk@${hk_version}#/Builtins.pkl\""
-		tail -n +3 "${config_path}"
-	} >"${temp_path}"
-
-	mv "${temp_path}" "${config_path}"
+	# Rewrite the versioned package URL wherever it appears
+	sed -i '' -E \
+		"s|package://github\.com/jdx/hk/releases/download/v[^/]+/hk@[^#]+#/|package://github.com/jdx/hk/releases/download/v${hk_version}/hk@${hk_version}#/|g" \
+		"${config_path}"
 }
 
 main "$@"
