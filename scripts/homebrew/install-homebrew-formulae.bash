049e85121cb24269bb743ae0ac57f46bf674b151
Isaac Halvorson <hello@hisaac.net>
2026-09-24 15:28:47 -0500

    Tear it down so we can build it back up! (#20)
    * Tear it down so we can build it back up!
    
    * fix lint errors

--- scripts/homebrew/install-homebrew-formulae.bash
+++ scripts/homebrew/install-homebrew-formulae.bash
@@ -1,8 +1,5 @@
 #!/bin/bash -euo pipefail
 
-# shellcheck source=../../roles/system_config/files/shell_profile/.profile
-source "${HOME}/.profile"
-
 function main() {
 	local -r formulae="${1:-${BREW_FORMULAE}}"
 
